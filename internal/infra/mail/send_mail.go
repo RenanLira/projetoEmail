dd4ecf75a73ca080a620b0842cd8fd80493d50d9
RenanLira <renanalmeida2015.ra@gmail.com>
2025-01-14 14:25:08 -0300

    implemented send mail

---
+++ internal/infra/mail/send_mail.go
@@ -0,0 +1,33 @@
+package mail
+
+import (
+	"os"
+	"projetoEmail/internal/domain/campaign"
+
+	"gopkg.in/gomail.v2"
+)
+
+func SendMail(campaign *campaign.Campaign) error {
+	provider := gomail.NewDialer(os.Getenv("EMAIL_SMTP"), 587, os.Getenv("EMAIL_USER"), os.Getenv("EMAIL_PASSWORD"))
+
+	emails := getEmailsFromContacts(campaign.Contacts)
+
+	m := gomail.NewMessage()
+	m.SetHeader("From", os.Getenv("EMAIL_USER"))
+	m.SetHeader("To", emails...)
+	m.SetHeader("Subject", campaign.Name)
+	m.SetBody("text/html", campaign.Content)
+
+	return provider.DialAndSend(m)
+}
+
+func getEmailsFromContacts(contacts []campaign.Contact) []string {
+
+	var emails []string
+
+	for _, contact := range contacts {
+		emails = append(emails, contact.Email)
+	}
+
+	return emails
+}
