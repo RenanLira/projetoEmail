acab64a6be289f1bd32000514283c6584b9a0020
RenanLira <renanalmeida2015.ra@gmail.com>
2025-01-22 13:26:23 -0300

    implemented worker

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,23 +1,56 @@
 package main
 
 import (
+	"fmt"
 	"projetoEmail/internal/domain/campaign"
 	"projetoEmail/internal/infra/database"
+	"projetoEmail/internal/infra/mail"
 
 	"github.com/joho/godotenv"
+	"gorm.io/gorm"
 )
 
-func main() {
+var (
+	db         *gorm.DB
+	repository database.CampaignRepository
+)
+
+func init() {
 	err := godotenv.Load()
 	if err != nil {
 		panic(err)
 	}
+}
 
-	db := database.NewDB()
+func main() {
 
-	repository := database.CampaignRepository{Db: db}
+	db = database.NewDB()
+	repository = database.CampaignRepository{Db: db}
 
+	service := &campaign.Service{Repository: &repository, SendMail: mail.SendMail}
+
+	campaigns := getCampaignsToBeSent()
+
+	for _, c := range campaigns {
+		fmt.Println("Sending campaign: ", c.Name)
+		
+		go func(c campaign.Campaign) {
+			err := service.SendMailAndUpdateStatus(&c)
+			if err != nil {
+				fmt.Println("Error sending campaign: ", c.Name)
+			}
+		}(c)
+	}
+
+}
+
+func getCampaignsToBeSent() []campaign.Campaign {
 	var campaigns []campaign.Campaign
-	repository.Db.Where("status = ?", campaign.Pending).Find(&campaigns)
+	tx := repository.Db.Preload("Contacts").Where("status = ?", campaign.Started).Where("date_part('minute', now()::timestamp - update_at::timestamp) > 3").Find(&campaigns)
 
+	if tx.Error != nil {
+		fmt.Println("Error fetching campaigns: ", tx.Error)
+	}
+
+	return campaigns
 }
